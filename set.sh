43f061742d68b50cdb8fc9082781a495bc8f49a9
uu <86598757+uu5007mp4@users.noreply.github.com>
2024-03-26 19:52:18 +0900

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -r p.py
 # URLからPythonスクリプトをダウンロード
-wget https://psannetwork.net/test/p.py
+wget https://github.com/uu5007mp4/BBS-Proxy/edit/main/test/Youtube%E4%BF%AE%E6%AD%A31.py
 
 # Create a Python virtual environment
 python3 -m venv venv
@@ -13,7 +13,7 @@ source venv/bin/activate
 pip install flask beautifulsoup4 requests
 
 # Run the Python script
-python3 p.py
+python3 Youtube%E4%BF%AE%E6%AD%A31.py
 
 # Deactivate the virtual environment
 deactivate
