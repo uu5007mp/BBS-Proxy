f4cc285abec82e49e805d1f60239bc1bec26d8be
uu <86598757+uu5007mp4@users.noreply.github.com>
2024-03-26 20:00:18 +0900

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -r p.py
 # URLからPythonスクリプトをダウンロード
-wget https://github.com/uu5007mp4/BBS-Proxy/edit/main/test/Youtube%E4%BF%AE%E6%AD%A31.py
+wget https://raw.githubusercontent.com/uu5007mp4/BBS-Proxy/main/test/Youtube%E4%BF%AE%E6%AD%A31.py?token=GHSAT0AAAAAACPPQVQSTJUL5VHPHJZKHXSAZQCW67Q
 
 # Create a Python virtual environment
 python3 -m venv venv
