41060524bc6f93d74481d6366c1a9d204e9bfd47
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-06-16 18:05:46 +0900

    Update set.sh
    Ultravioletセットアップsh

--- set.sh
+++ set.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-rm -r p.py
-# URLからPythonスクリプトをダウンロード
-wget https://raw.githubusercontent.com/uu5007mp4/BBS-Proxy/main/test/Youtube%E4%BF%AE%E6%AD%A31.py?token=GHSAT0AAAAAACPPQVQSTJUL5VHPHJZKHXSAZQCW67Q
 
-# Create a Python virtual environment
-python3 -m venv venv
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
 
-# Activate the virtual environment
-source venv/bin/activate
+check_and_install() {
+    dpkg -s $1 &> /dev/null
+    if [ $? -ne 0 ]; then
+        apt install -y $1
+    fi
+}
 
-# Install necessary packages
-pip install flask beautifulsoup4 requests
+apt update
 
-# Run the Python script
-python3 Youtube%E4%BF%AE%E6%AD%A31.py
+check_and_install git
+check_and_install npm
 
-# Deactivate the virtual environment
-deactivate
+git clone https://github.com/hirotomoki12345/Ultraviolet-App.git
+
+cd Ultraviolet-App
+
+npm install
+npm start
