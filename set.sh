186ef4509fd245714330cd1fb8417ebaddd57677
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-06-17 04:32:11 +0900

    Update set.sh
    その他ディストリビューションをサポート

--- set.sh
+++ set.sh
@@ -5,21 +5,39 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-check_and_install() {
-    dpkg -s $1 &> /dev/null
-    if [ $? -ne 0 ]; then
-        apt install -y $1
-    fi
+install_debian() {
+    dpkg -s "$1" &> /dev/null || apt install -y "$1"
 }
 
-apt update
+install_arch() {
+    pacman -Qi "$1" &> /dev/null || pacman -S --noconfirm "$1"
+}
 
-check_and_install git
-check_and_install npm
+install_redhat() {
+    rpm -q "$1" &> /dev/null || yum install -y "$1"
+}
+
+install_packages() {
+    for package in "$@"; do
+        $1 "$package"
+    done
+}
+
+if [ -f /etc/debian_version ]; then
+    apt update
+    install_packages install_debian git npm
+elif [ -f /etc/arch-release ]; then
+    pacman -Syu --noconfirm
+    install_packages install_arch git npm
+elif [ -f /etc/redhat-release ]; then
+    yum update -y
+    install_packages install_redhat git npm
+else
+    echo "Unsupported distribution"
+    exit 1
+fi
 
 git clone https://github.com/uu5007mp4/BBS-Proxy.git
-
 cd BBS-Proxy
-
 npm install
 npm start
